84249c4dd3d4bf6fc65d411516f71551f8d6d20f
austin.hong <manofhee@gmail.com>
2024-12-15 17:29:22 -0800

    update queue

--- embedded-c/queue.c
+++ embedded-c/queue.c
@@ -3,53 +3,50 @@
 #include <stdbool.h>
 #include <math.h>
 
-#define QUEUE_COUNT 10
+#define QUEUE_COUNT 100
 
-typedef struct
-{
-    int item[QUEUE_COUNT];
+typedef struct {
+    int data[QUEUE_COUNT];
     int head;
     int tail;
 } Queue;
 
-int queue_inx_inc(int val)
-{
-    return (val + 1) % QUEUE_COUNT;
+int queue_inc(int index) {
+    return (index + 1) % QUEUE_COUNT;
 }
 
-void queue_init(Queue *q)
-{
+void queue_init(Queue* q) {
     q->head = q->tail = 0;
 }
 
-bool queue_is_empty(Queue *q)
-{
+bool queue_is_empty(Queue* q) {
     return q->head == q->tail;
 }
 
-bool queue_is_full(Queue *q)
-{
-    return queue_inx_inc(q->head) == q->tail;
+bool queue_is_full(Queue* q) {
+    return queue_inc(q->head) == q->tail;
 }
 
-bool queue_push(Queue *q, int val)
-{
-    if (queue_is_full(q)) {
-        printf("queue push fail, queue full\n");
+bool queue_push(Queue* q, int val) {
+    if(queue_is_full(q)) {
+        printf("queue_push failed due to queue full\n");
         return false;
     }
-    q->head = queue_inx_inc(q->head);
-    q->item[q->head] = val;
+    q->head = queue_inc(q->head);
+    q->data[q->head] = val;
     return true;
 }
 
-bool queue_pop(Queue *q, int *val)
-{
-    if (queue_is_empty(q)) {
-        printf("queue pop fail, queue empty\n");
+bool queue_pop(Queue* q, int* pval) {
+    if(queue_is_empty(q)) {
+        printf("queue_pop failed due to queue empty\n");
         return false;
     }
-    *val = q->item[q->tail];
-    q->tail = queue_inx_inc(q->tail);
+    *pval = q->data[q->tail];
+    q->tail = queue_inc(q->tail);
     return true;
+}
+
+int queue_used(Queue* q) {
+    return ((q->head + QUEUE_COUNT) - q->tail) % QUEUE_COUNT;
 }
